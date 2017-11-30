b9c6af344fce8fd8e55ff847ba59fb6098be2ae9
Joshua Lukemire <joshlukemire@MacBook-Pro.local>
2017-11-30 15:57:55 -0500

    red

--- example_code.R
+++ example_code.R
@@ -5,7 +5,10 @@ beta <- runif(2, -3, 3)
 y <- beta[1] + beta[2] * x   + rnorm(n)
 
 # Plot
-plot(x, y, pch=20, col)
+plot(x, y, pch=20, col="red")
+
+# Histogram of y values   
+hist(y)
 
 # Fit a model
 g <- lm(y ~ x)
